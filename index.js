6cc5389297504c9c525f8189bde41c3a2fa51e13
Dhuha Hasan <duha_9@yahoo.com>
2019-09-25 14:03:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array,eventname){
+  let thankYou=[];
+  for(let i=0;i<array.length;i++){
+    thankYou.push("Thank you, ${array[i]}+ ,for the wonderful ${eventname} gift!")
+  }
+  return thankYou;
+}
+
+function countdown(count){
+  while(count>0){
+    console.log(count);
+    count=count-1;
+  }
+  
+}
+const names = ["ahmed", "noor", "muna"];
+console.log(writeCards(names,"birthday"));
